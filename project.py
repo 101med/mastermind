983040042ba06c8bbcf694cc02f622fff476ee78
101med <med@void.localdomain>
2023-09-12 21:47:44 +0100

    Imporve

--- project.py
+++ project.py
@@ -3,6 +3,9 @@
 import curses
 
 
+BORDER = ["|", "|", "-", "-", "+", "+", "+", "+"]
+
+
 class Screen:
     def __init__(self, stdscr: curses.window, lines: int, cols: int) -> None:
         self.stdscr = stdscr
@@ -20,10 +23,6 @@ class Window(Screen):
         self.X = x if x is not None else (curses.COLS - self.COLS) // 2
         self.window = curses.newwin(self.LINES, self.COLS, self.Y, self.X)
 
-    @property
-    def border(self) -> list[str]:
-        return ["|", "|", "-", "-", "+", "+", "+", "+"]
-
 
 class Pad(Screen):
     def __init__(self, stdscr: curses.window, lines: int, cols: int) -> None:
@@ -31,50 +30,17 @@ class Pad(Screen):
         self.window = curses.newwin(self.LINES, self.COLS)
 
 
-class BoardWindow(Window):
-    def __init__(self, stdscr: curses.window, lines: int, cols: int, y=None, x=None) -> None:
-        super().__init__(stdscr, lines, cols, y, x)
-
-    def show_board(self) -> None:
-        ...
-
-    def _draw(self) -> str:
-        ...
-
-
-class InputWindow(Window):
-    def __init__(self, stdscr: curses.window, lines: int, cols: int, y=None, x=None) -> None:
-        super().__init__(stdscr, lines, cols, y,x)
-
-    def handel_input(self) -> list[int]:
-        ...
-
-
-class HintWindow(Window):
-    def __init__(self, stdscr: curses.window, lines: int, cols: int, y=None, x=None) -> None:
-        super().__init__(stdscr, lines, cols, y,x)
-
-    def show_message(self, message: ValueError) -> None:
-        ...
-
-    def show_game_over(self) -> None:
-        ...
-
-
-class HelpPad(Pad):
-    def __init__(self, stdscr: curses.window, lines: int, cols: int) -> None:
-        super().__init__(stdscr, lines, cols)
-
-    def scroll(self) -> None:
-        ...
+class GameUI:
+    def __init__(self, stdscr: curses.window) -> None:
+        self.MAIN = Window(stdscr, lines=curses.LINES - 1, cols=curses.COLS)
+        self.BOARD = Window(stdscr, lines=15, cols=21)
+        self.INPUT = Window(stdscr, lines=1, cols=5, y=1, x=1)
+        self.HINT = Window(stdscr, lines=7, cols=40)
+        self.HELP = Pad(stdscr, lines=40, cols=46)
 
 
 def main(stdscr):
-    MAIN = Window(stdscr, lines=curses.LINES - 1, cols=curses.COLS)
-    BOARD = BoardWindow(stdscr, lines=15, cols=21)
-    HINT = HintWindow(stdscr, lines=7, cols=40)
-    HELP = HelpPad(stdscr, lines=40, cols=46)
-    INPUT = InputWindow(stdscr, lines=1, cols=5, y=1, x=1)
+    ui = GameUI(stdscr)
 
 
 if __name__ == "__main__":
