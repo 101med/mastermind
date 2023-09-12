9f0b146fb9e1dd76afbc097548183f0d5585988d
med <med@arch.localdomain>
2023-09-12 22:02:55 +0100

    A

--- project.py
+++ project.py
@@ -32,11 +32,41 @@ class Pad(Screen):
 
 class GameUI:
     def __init__(self, stdscr: curses.window) -> None:
+        curses.curs_set(0)
+
+        self.stdscr = stdscr
         self.MAIN = Window(stdscr, lines=curses.LINES - 1, cols=curses.COLS)
         self.BOARD = Window(stdscr, lines=15, cols=21)
         self.INPUT = Window(stdscr, lines=1, cols=5, y=1, x=1)
         self.HINT = Window(stdscr, lines=7, cols=40)
         self.HELP = Pad(stdscr, lines=40, cols=46)
+
+        self.INPUT.window.keypad(True)
+        self.HELP.window.keypad(True)
+
+    def show_menu(self) -> None:
+        title = "+MASTERMIND+"
+        keys = "0-9 Guess. Enter Confirm. H Toggle help. Q quit."
+
+        title_x = (curses.COLS - len(title)) // 2
+        keys_x = (curses.COLS - len(keys)) // 2
+
+        self.MAIN.window.border(*BORDER)
+
+        self.stdscr.addstr(0, title_x, title)
+        self.stdscr.chgat(0, title_x + 1, len(title) - 2, curses.A_BOLD)
+
+        self.stdscr.addstr(curses.LINES - 1, keys_x, keys)
+        self.stdscr.chgat(curses.LINES - 1, keys_x + 0, 3, curses.A_BOLD)
+        self.stdscr.chgat(curses.LINES - 1, keys_x + 11, 5, curses.A_BOLD)
+        self.stdscr.chgat(curses.LINES - 1, keys_x + 26, 1, curses.A_BOLD)
+        self.stdscr.chgat(curses.LINES - 1, keys_x + 41, 1, curses.A_BOLD)
+
+        self.stdscr.refresh()
+
+    def show_board(self) -> None:
+        ...
+
 
 
 def main(stdscr):
